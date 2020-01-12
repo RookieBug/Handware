62b02012573852f69c5fd19b060e0cd0cd8b5866
TulLingS <181895342948@163.com>
2020-01-12 20:15:05 +0800

    一些常量数据的设计

---
+++ src/main/java/xyz/tulling/hardware/util/Constant.java
@@ -0,0 +1,7 @@
+package xyz.tulling.hardware.util;
+
+public class Constant {
+    public static final String CARD_ID = "card_id";
+    public static final String EMPLOYEE_ID = "employee_id";
+    public static final String HARDWARE_ID = "hardware_id";
+}
